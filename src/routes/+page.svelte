2a445cb871343b9177e622f232b4c73609da2306
opensas <opensas@gmail.com>
2023-03-14 15:04:32 -0300

    fix label

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	import { goto } from "$app/navigation";
 </script>
 
-<button on:click={() => goto('/redirect')}> goto page that redirects to external url: goto('/redirect') (debugger error)</button>
+<button on:click={() => goto('/redirect')}> goto page that redirects to external url: goto('/redirect') (debugger error!!!)</button>
 
 <br />
 
-<button on:click={() => window.location.href = '/redirect'}> window.location.href to page that redirect to external url: goto('/redirect') (debugger error)</button>
+<button on:click={() => window.location.href = '/redirect'}> window.location.href to page that redirect to external url: goto('/redirect') (OK!!!)</button>
